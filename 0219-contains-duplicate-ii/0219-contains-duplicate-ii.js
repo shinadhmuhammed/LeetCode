ef8a52f1a8ebe19a6cc0172346f995673f2bae7d
muhammed shinadh mk <141395903+shinadhmuhammed@users.noreply.github.com>
2024-03-20 12:11:29 +0530

    Time: 82 ms (85.88%), Space: 72.5 MB (37.56%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+     let map=new Map()
+        for(var i=0;i<nums.length;i++){
+           if( map.has(nums[i]) && Math.abs(map.get(nums[i])-i)<=k){
+            return true
+           }else{
+            map.set(nums[i],i)
+           }
+        }
+        return false
+        }
+        
+        
+    
