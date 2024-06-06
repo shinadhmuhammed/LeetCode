bd44935e76de715cbb2cfc9f66cca06af5af26ce
muhammed shinadh mk <141395903+shinadhmuhammed@users.noreply.github.com>
2024-06-06 20:40:27 +0530

    Time: 63 ms (73.36%), Space: 51.4 MB (89.71%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.js
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.js
@@ -3,16 +3,5 @@
  * @return {number[]}
  */
 var sortArrayByParity = function(nums) {
-    let arr=[]
-    for(var i=0;i<=nums.length;i++){
-        if(nums[i] % 2 === 0){
-              arr.push(nums[i])
-        }
-    }
-     for (var i = 0; i < nums.length; i++) {
-        if (nums[i] % 2 !== 0) {
-            arr.push(nums[i]);
-        }
-    }
-    return arr
+    return nums.filter((num)=>num%2 === 0).concat(nums.filter((num) => num%2 !== 0))
 };
