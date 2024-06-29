afe00e6d394a3ae130289884c70125e1dfee2741
muhammed shinadh mk <141395903+shinadhmuhammed@users.noreply.github.com>
2024-06-29 15:28:26 +0530

    Time: 63 ms (60.09%), Space: 53.2 MB (49.04%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.js
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -4,9 +4,9 @@
  */
 var isPalindrome = function(s) {
     if(s.length==0) return true
-     s=s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase()
-  
+     s=s.replace(/[^a-zA-Z0-9]/g,"").toLowerCase()
 
     return s===s.split('').reverse('').join('')
+   
      
 };
