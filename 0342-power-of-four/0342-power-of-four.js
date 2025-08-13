27f5c9efce44a41900d50b3e04acecf718ab02e7
muhammed shinadh mk <141395903+shinadhmuhammed@users.noreply.github.com>
2025-08-13 11:16:13 +0530

    Time: 0 ms (100%), Space: 57.5 MB (10.39%) - LeetHub

--- 0342-power-of-four/0342-power-of-four.js
+++ 0342-power-of-four/0342-power-of-four.js
@@ -2,9 +2,10 @@
  * @param {number} n
  * @return {boolean}
  */
-var isPowerOfFour = function(n) {
-        if(n%1!=0 || n==0) return false
-        if(n==1) return true
-        return isPowerOfFour(n/4)
-
+var isPowerOfFour = function (n) {
+    if (n < 1) return false
+    while (n % 4 === 0) {
+        n = n / 4
+    }
+    return n === 1
 };
