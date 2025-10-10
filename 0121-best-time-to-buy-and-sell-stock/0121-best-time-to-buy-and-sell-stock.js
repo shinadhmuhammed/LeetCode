97329b1eb0a1c205e78da61d4d50d92d191d7bf8
muhammed shinadh mk <141395903+shinadhmuhammed@users.noreply.github.com>
2025-10-10 13:14:14 +0530

    Time: 2 ms (82.14%), Space: 65.9 MB (29.69%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.js
@@ -2,15 +2,18 @@
  * @param {number[]} prices
  * @return {number}
  */
-var maxProfit = function(prices) {
-    let min= prices[0]
-    let max = 0
-    for (var i=1;i<=prices.length-1;i++){
-            if(prices[i] < min){
-                min=prices[i]
-            }else{
-                max=Math.max(max, prices[i] - min)
-            }
+var maxProfit = function (prices) {
+    let maxProfit = 0
+    let min = prices[0]
+
+    for (let i = 1; i < prices.length; i++) {
+        if (prices[i] - min > maxProfit) {
+            maxProfit = prices[i] - min
+        }
+
+        if (prices[i] < min) {
+            min = prices[i]
+        }
     }
-    return max
+    return maxProfit
 };
