7f14a24810b3e301a827bf1532af944ad7c4fcd1
muhammed shinadh mk <141395903+shinadhmuhammed@users.noreply.github.com>
2025-11-03 22:22:05 +0530

    Time: 144 ms (30.26%), Space: 81 MB (52.76%) - LeetHub

---
+++ 0948-sort-an-array/0948-sort-an-array.js
@@ -0,0 +1,29 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+
+var sortArray = function (nums) {
+    if (nums.length <= 1) return nums
+    let mid = Math.floor(nums.length / 2)
+    let left = sortArray(nums.slice(0, mid))
+    let right = sortArray(nums.slice(mid))
+    return mergeArray(left, right)
+}
+
+let mergeArray = function (left, right) {
+        let i=0
+        let j=0
+        let res = []
+
+        while(i<left.length && j < right.length){
+            if(left[i] < right[j]){
+                res.push(left[i])
+                i++
+            }else{
+                res.push(right[j])
+                j++
+            }
+        }
+        return [...res,...left.slice(i),...right.slice(j)]
+}
